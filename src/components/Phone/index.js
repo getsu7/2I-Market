1569f1950d0edaad41a90e6eab80b72361767b5c
Tomus69 <dacunha.tom@gmail.com>
2022-09-30 12:42:04 +0200

    Add phone datas + display phone cards

--- src/components/Phone/index.js
+++ src/components/Phone/index.js
@@ -1,9 +1,11 @@
 import insertElement from '../../commons/functions/insertElement.js';
+import getPhoneCards from './PhoneCard.js';
 
 export default function getPhone() {
     const root = document.querySelector('#main-content');
     const title = insertElement('h1', 'content', root);
     title.appendChild(document.createTextNode('Smartphone'));
     title.style.textAlign = 'center';
+    getPhoneCards(root);
     return root;
 }
---
+++ src/models/Phone.js
@@ -0,0 +1,15 @@
+export default class Phone {
+    id;
+    brand;
+    title;
+    img;
+    price;
+
+    constructor(id, brand, title, img, price) {
+        this.id = id;
+        this.brand = brand;
+        this.title = title;
+        this.img = img;
+        this.price = price;
+    }
+};
